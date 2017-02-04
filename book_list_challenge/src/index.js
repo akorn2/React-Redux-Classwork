8dd212d0631a3d4c415f7a07be0c1f44aaf0ef70
akorn2 <schleichkorn@gmail.com>
2017-02-04 13:56:14 -0600

    Recreating tutorials project from memory

---
+++ book_list_challenge/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+
+import App from './components/app';
+// import reducers from './reducers';
+console.log('test');
+
+ReactDOM.render(
+<div>
+    <App />
+</div>
+  , document.querySelector('.container'));
